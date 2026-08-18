ea03f136396d3fd05f97c0ada2fe28fa88eec743
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2026-08-18 21:34:57 +0200

    model cart

---
+++ internal/models/cart.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/gorm"
+
+type Cart struct {
+    gorm.Model
+    UserID uint
+    Items  []CartItem
+}
+
+type CartItem struct {
+    gorm.Model
+    CartID    uint
+    ProductID uint
+    Product   Product
+    Quantity  int
+}
